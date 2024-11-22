bb15c8a9b0837a7e8b16f087b53e9f0f896b57d6
ChandraSai1702 <chandrasai@sigmoidanalytics.com>
2024-11-22 14:55:15 +0530

    .gitignore

--- modules/ssh/ssh.tf
+++ modules/ssh/ssh.tf
@@ -6,8 +6,4 @@ resource "tls_private_key" "ssh" {
 resource "aws_key_pair" "ec2_key" {
   key_name   = "terraform-key"
   public_key = tls_private_key.ssh.public_key_openssh
-
-  lifecycle {
-    prevent_destroy = false
-  }
 }
